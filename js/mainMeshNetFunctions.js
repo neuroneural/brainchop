b27a7d5045236a324a70116b53c992c58a373112
Mohamed <mmasoud2@outlook.com>
2022-05-13 03:52:06 -0400

    Update

--- js/mainMeshNetFunctions.js
+++ js/mainMeshNetFunctions.js
@@ -2145,7 +2145,7 @@ isOnline= () => {
 *
 */
 
-  getBrowserLocationInfo = () => {
+  getBrowserLocationInfo = () => {                   
       let LocationDataObj = {}; 
       
       if(isOnline()){
@@ -2153,31 +2153,31 @@ isOnline= () => {
               $.ajax({
                     url: 'https://api.ipregistry.co/?key=tryout',
                     async: false,
-                    dataType: 'json',
-                    success: function (response) {
-                      LocationDataObj = {Country: response.location.country.name, Region: response.location.region.name, City: response.location.city};
-                    }
-                  });
+                    dataType: "json",
+                    success: function(response) {
+                        LocationDataObj = {Country: response.location.country.name, Region: response.location.region.name, City: response.location.city, latitude: response.location.latitude, longitude: response.location.longitude};                        
+                    },
+                    error: function(XMLHttpRequest, textStatus, errorThrown) { 
+                        // alert("Status: " + textStatus); alert("Error: " + errorThrown); 
+                          $.ajax({
+                                url: "https://geolocation-db.com/json/",
+                                async: false,
+                                dataType: 'json',
+                                success: function (response) {
+                                  LocationDataObj = {Country: response.country_name, Region: response.state, City: response.city, latitude: response.latitude, longitude: response.longitude};
+                                },
+                                error: function(XMLHttpRequest, textStatus, errorThrown) { 
+                                    console.log("Resource for browser info not available ");
+                                }  
+                          });
+                    }                     
+              });
           } catch(err) {
-                console.log("Resource for browser info not available ");
-                try {
-                      $.ajax({
-                            url: "https://geolocation-db.com/jsonp",
-                            jsonpCallback: "callback",
-                            dataType: "jsonp",
-                            success: function(location) {
-                                LocationDataObj = {Country: location.country_name, Region: location.state, City: location.city};
-                            }
-                      });
-                } catch(err2) {
                     console.log("Online resources for browser info currently not available ");
-                    return null;
-                }
-
-
           }
-      }
 
+     }
+    
        return LocationDataObj;
 }
 
@@ -2286,6 +2286,65 @@ submitTiming2GoogleSheet = (dataObj) => {
      }
 
 }
+
+
+/**
+* Function to submit data to google sheet
+*
+* @since 1.0.0
+*
+*/
+
+updateHitData= () => {    
+
+      if(isOnline()){ 
+
+            let geoData = getBrowserLocationInfo();
+            if(geoData) {
+                hitData["Country"] = geoData["Country"];
+                hitData["State"] = geoData["Region"];
+                hitData["City"] = geoData["City"];
+                hitData["Lat"] = geoData["latitude"];
+                hitData["Lng"] = geoData["longitude"];
+            } else {
+                hitData["Country"] = "";
+                hitData["State"] = "";
+                hitData["City"] = "";
+                hitData["Lat"] = null;
+                hitData["Lng"] = null;
+            }
+
+            let today = new Date();
+            hitData["Date"] = parseInt(today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();   
+            hitData["Time"] = checkZero(today.getHours()) + ":" + checkZero(today.getMinutes()) + ":" + checkZero(today.getSeconds());
+
+            // -- Fill form with data to submit
+            Object.keys(hitData).forEach(dataKey =>{
+                 document.getElementById(dataKey).value = hitData[dataKey];
+            }) 
+
+            //-- Settings of submission
+            const scriptURL = 'https://script.google.com/macros/s/AKfycbyQPabYEzMGWx2fj4aihuDlk3khg5s64w_nZlYgd6K0LB6XpolK9j0OPYlSay0A8PksYg/exec'
+            const form = document.forms['google-sheet']
+          
+            //-- Add event handler to the form.
+            form.addEventListener('submit', e => {
+                  e.preventDefault()
+                  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                    .then(response => console.log("time recorded"))
+                    .catch(error => console.log('Error!', error.message))
+            })    
+
+            //-- Submit the form 
+            document.getElementById("SubmitHitData").click();   
+
+      } else {
+         console.log(" Offline Mode ")    
+                
+      }
+
+}
+
 
 /**
 * For adjust time by adding 0  
