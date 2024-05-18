cba17da95b63a3ba0a2f0f20565ce421a225bf48
Mmasoud1 <36082112+Mmasoud1@users.noreply.github.com>
2024-05-18 18:49:20 +0000

    Deploying to demo from @ neuroneural/brainchop@5bbe0900915bae6419ef56de1bb6692101450304 🚀

--- v3/js/libs/webix/sources/views/checksuggest.js
+++
@@ -1,7 +0,0 @@
-
-import {protoUI} from "../ui/core";
-import template from "../views/template";
-
-protoUI({ name:"checksuggest", defaults:{
-	template:"GPL version doesn't support checksuggest <br> You need Webix PRO"
-}}, template.view);
--- v3/js/libs/webix/sources/webix/patterns.js
+++
@@ -1,7 +0,0 @@
-const patterns = {
-	phone:{ mask:"+# (###) ###-####", allow:/[0-9]/g },
-	card: { mask:"#### #### #### ####", allow:/[0-9]/g },
-	date: { mask:"####-##-## ##:##", allow:/[0-9]/g }
-};
-
-export default patterns;
