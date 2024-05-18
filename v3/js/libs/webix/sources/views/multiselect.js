de248cb100d561c7b8b28183a81105e0487a1c9b
Mmasoud1 <36082112+Mmasoud1@users.noreply.github.com>
2024-05-18 06:14:42 +0000

    Deploying to demo from @ neuroneural/brainchop@cf981f9a05c602250eb62f0cd39249f2e369d211 🚀

--- v3/js/libs/webix/sources/views/multiselect.js
+++
@@ -1,7 +0,0 @@
-
-import {protoUI} from "../ui/core";
-import template from "../views/template";
-
-protoUI({ name:"multiselect", defaults:{
-	template:"GPL version doesn't support multiselect <br> You need Webix PRO"
-}}, template.view);
