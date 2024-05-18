d9d72f9888ca556fddb89da3ab283858e30daceb
Mmasoud1 <36082112+Mmasoud1@users.noreply.github.com>
2024-05-18 06:04:21 +0000

    Deploying to demo from @ neuroneural/brainchop@3c45eab062547f6aec577721c6917eb605195e53 🚀

--- v3/js/libs/webix/sources/views/excelbar.js
+++
@@ -1,7 +0,0 @@
-
-import {protoUI} from "../ui/core";
-import template from "../views/template";
-
-protoUI({ name:"excelbar", defaults:{
-	template:"GPL version doesn't support excelbar <br> You need Webix PRO"
-}}, template.view);
