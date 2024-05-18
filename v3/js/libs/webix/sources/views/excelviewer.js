429953b818e7e865db6def73daed7fe98050cf12
Mmasoud1 <36082112+Mmasoud1@users.noreply.github.com>
2024-05-18 04:18:24 +0000

    Deploying to demo from @ neuroneural/brainchop@f018c6785479f6e0f342b94bb92d780d853dd80b 🚀

--- v3/js/libs/webix/sources/views/excelviewer.js
+++
@@ -1,7 +0,0 @@
-
-import {protoUI} from "../ui/core";
-import template from "../views/template";
-
-protoUI({ name:"excelviewer", defaults:{
-	template:"GPL version doesn't support excelviewer <br> You need Webix PRO"
-}}, template.view);
