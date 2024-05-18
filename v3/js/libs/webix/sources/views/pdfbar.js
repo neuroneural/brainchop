c92e31e1cc598d435f3c5a79fdbe01917751a18c
Mmasoud1 <36082112+Mmasoud1@users.noreply.github.com>
2024-05-18 04:42:40 +0000

    Deploying to demo from @ neuroneural/brainchop@95a1dbe61763060c88e59dc1b10fc9583b09a070 🚀

--- v3/js/libs/webix/sources/views/pdfbar.js
+++
@@ -1,7 +0,0 @@
-
-import {protoUI} from "../ui/core";
-import template from "../views/template";
-
-protoUI({ name:"pdfbar", defaults:{
-	template:"GPL version doesn't support pdfbar <br> You need Webix PRO"
-}}, template.view);
