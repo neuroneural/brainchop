5ffbae8d2e30171527bd4bca7e5e796835aafea6
Mmasoud1 <36082112+Mmasoud1@users.noreply.github.com>
2024-05-29 16:57:48 +0000

    Deploying to demo from @ neuroneural/brainchop@dc18f9dcb0c44911bf80c84e7b3e8ce73693335b 🚀

--- v3/js/libs/webix/sources/views/datalayout.js
+++
@@ -1,7 +0,0 @@
-
-import {protoUI} from "../ui/core";
-import template from "../views/template";
-
-protoUI({ name:"datalayout", defaults:{
-	template:"GPL version doesn't support datalayout <br> You need Webix PRO"
-}}, template.view);
--- v3/js/libs/webix/sources/views/panel.js
+++
@@ -1,7 +0,0 @@
-
-import {protoUI} from "../ui/core";
-import template from "../views/template";
-
-protoUI({ name:"panel", defaults:{
-	template:"GPL version doesn't support panel <br> You need Webix PRO"
-}}, template.view);
