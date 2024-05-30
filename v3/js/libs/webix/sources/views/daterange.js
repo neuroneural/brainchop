b2288260cd29cd8070843d9646a206e5b420cc6d
Mmasoud1 <36082112+Mmasoud1@users.noreply.github.com>
2024-05-30 03:43:16 +0000

    Deploying to demo from @ neuroneural/brainchop@d447af1f06a89cec6daaf9ead752934baefdd8e9 🚀

--- v3/js/libs/webix/sources/views/daterange.js
+++
@@ -1,7 +0,0 @@
-
-import {protoUI} from "../ui/core";
-import template from "../views/template";
-
-protoUI({ name:"daterange", defaults:{
-	template:"GPL version doesn't support daterange <br> You need Webix PRO"
-}}, template.view);
--- v3/js/libs/webix/sources/views/portlet.js
+++
@@ -1,7 +0,0 @@
-
-import {protoUI} from "../ui/core";
-import template from "../views/template";
-
-protoUI({ name:"portlet", defaults:{
-	template:"GPL version doesn't support portlet <br> You need Webix PRO"
-}}, template.view);
