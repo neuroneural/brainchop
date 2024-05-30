59475ab26f00e8deef0302a93d024a98ff10ea4e
Mmasoud1 <36082112+Mmasoud1@users.noreply.github.com>
2024-05-30 18:52:33 +0000

    Deploying to demo from @ neuroneural/brainchop@e36437a1b18a0d36128026286f9d6e2434d11df1 🚀

--- v3/js/libs/webix/sources/core/customscroll.js
+++
@@ -1,1 +0,0 @@
-const temp = null; export default temp;
--- v3/js/libs/webix/sources/views/multisuggest.js
+++
@@ -1,7 +0,0 @@
-
-import {protoUI} from "../ui/core";
-import template from "../views/template";
-
-protoUI({ name:"multisuggest", defaults:{
-	template:"GPL version doesn't support multisuggest <br> You need Webix PRO"
-}}, template.view);
