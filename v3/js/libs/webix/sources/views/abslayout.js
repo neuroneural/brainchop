942feb551ba6d44a28312bcc1e95e5223a807b5c
Mmasoud1 <36082112+Mmasoud1@users.noreply.github.com>
2024-06-01 21:48:30 +0000

    Deploying to demo from @ neuroneural/brainchop@fff1ba08570ce4603b67d5f1ed964462207bbf84 🚀

--- v3/js/libs/webix/sources/views/abslayout.js
+++
@@ -1,7 +0,0 @@
-
-import {protoUI} from "../ui/core";
-import template from "../views/template";
-
-protoUI({ name:"abslayout", defaults:{
-	template:"GPL version doesn't support abslayout <br> You need Webix PRO"
-}}, template.view);
