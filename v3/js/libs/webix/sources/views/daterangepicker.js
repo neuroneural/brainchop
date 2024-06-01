cc8fcae54f80c1374bb44d8c6496a12c95661210
Mmasoud1 <36082112+Mmasoud1@users.noreply.github.com>
2024-06-01 21:31:16 +0000

    Deploying to demo from @ neuroneural/brainchop@1fc13a044ed615c77fda0709da7b374cf4169025 🚀

--- v3/js/libs/webix/sources/views/daterangepicker.js
+++
@@ -1,7 +0,0 @@
-
-import {protoUI} from "../ui/core";
-import template from "../views/template";
-
-protoUI({ name:"daterangepicker", defaults:{
-	template:"GPL version doesn't support daterangepicker <br> You need Webix PRO"
-}}, template.view);
