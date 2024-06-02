3f897d5a00fcc4faf93bd61e00208398b008e1e4
mmasoud1 <mmasoud2@outlook.com>
2024-06-02 17:34:03 -0400

    cleaning code

--- v3/js/libs/webix/sources/views/filter.js
+++
@@ -1,7 +0,0 @@
-
-import {protoUI} from "../ui/core";
-import template from "../views/template";
-
-protoUI({ name:"filter", defaults:{
-	template:"GPL version doesn't support filter <br> You need Webix PRO"
-}}, template.view);
--- v3/js/libs/webix/sources/views/multicombo.js
+++
@@ -1,7 +0,0 @@
-
-import {protoUI} from "../ui/core";
-import template from "../views/template";
-
-protoUI({ name:"multicombo", defaults:{
-	template:"GPL version doesn't support multicombo <br> You need Webix PRO"
-}}, template.view);
