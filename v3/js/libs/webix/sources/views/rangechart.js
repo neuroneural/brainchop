1a20c2ef1ee6f74c8a13e52adb0d256baea4364c
Mmasoud1 <36082112+Mmasoud1@users.noreply.github.com>
2024-06-02 08:12:53 +0000

    Deploying to demo from @ neuroneural/brainchop@38a2ab8588ae9d1440d9abb9b11647180145e2b1 🚀

--- v3/js/libs/webix/sources/views/rangechart.js
+++
@@ -1,7 +0,0 @@
-
-import {protoUI} from "../ui/core";
-import template from "../views/template";
-
-protoUI({ name:"rangechart", defaults:{
-	template:"GPL version doesn't support rangechart <br> You need Webix PRO"
-}}, template.view);
