ceeaa1586f4333de9c440cc300fae9da95ba4e53
Sergey Plis <981291+sergeyplis@users.noreply.github.com>
2026-02-24 10:42:03 -0500

    Update vite.config.js
    trying another root to quickly save brainchop

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
   root: '.',
-  base: '/brainchop/',
+  base: '/',
   plugins: [
     // Generate gzip compressed files
     viteCompression({
